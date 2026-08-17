5221ca257a355b2be14e64b1b506bd07fd933b48
ansarisuhel4505 <ansarisuhel4505@gmail.com>
2026-08-17 22:46:43 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // यह लाइन react-pdf को Next.js के Server-Side Rendering से बाहर कर देगी
-  serverComponentsExternalPackages: ['pdf-lib', 'react-pdf'],
-}
-
-module.exports = nextConfig
