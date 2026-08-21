17c5dafa6956d4d632381f66591fbc061640bc7f
ansarisuhel4505 <ansarisuhel4505@gmail.com>
2026-08-21 14:52:20 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css';
+import { ClerkProvider } from '@clerk/next5'; // Agar clerk package installed hai
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 }
