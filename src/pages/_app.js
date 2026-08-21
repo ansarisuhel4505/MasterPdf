1c138b0f54a34acba232c44b294fa616ef987181
ansarisuhel4505 <ansarisuhel4505@gmail.com>
2026-08-21 17:30:22 +0530

    Refactor MyApp component structure

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,18 +3,18 @@ import { ClerkProvider } from '@clerk/nextjs';
 import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <ClerkProvider {...pageProps}>
-      <Head>
-        {/* Google aur Browser ko Logo batane ka code */}
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="shortcut icon" href="/favicon.svg" />
-        <link rel="apple-touch-icon" href="/favicon.svg" />
-        <meta name="theme-color" content="#E5322D" />
-      </Head>
-      
-      {/* Tumhari saari website yahan load hoti hai */}
-      <Component {...pageProps} />
-    </ClerkProvider>
-  );
+  return (
+    <ClerkProvider {...pageProps}>
+      <Head>
+        {/* Google aur Browser ko Logo batane ka code */}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
+        <meta name="theme-color" content="#E5322D" />
+      </Head>
+      
+      {/* Tumhari saari website yahan load hoti hai */}
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 }
