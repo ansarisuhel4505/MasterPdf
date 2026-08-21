8cd4f185ac9ae502e8db33c70c7ab9585b5f371e
ansarisuhel4505 <ansarisuhel4505@gmail.com>
2026-08-21 14:43:26 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
-import '@/styles/globals.css'; // Make sure path is correct based on your setup
-
+import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
   return (
     <>
