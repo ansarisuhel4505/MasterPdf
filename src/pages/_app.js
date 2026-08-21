048d54963ac856f980f19a6fd2b698928e763d04
ansarisuhel4505 <ansarisuhel4505@gmail.com>
2026-08-21 14:54:29 +0530

    Remove ClerkProvider from MyApp component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,5 @@
 import '@/styles/globals.css';
-import { ClerkProvider } from '@clerk/next5'; // Agar clerk package installed hai
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
-    </ClerkProvider>
-  );
+  return <Component {...pageProps} />;
 }
