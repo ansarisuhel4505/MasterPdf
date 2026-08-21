cf22072ef8370bbdaca0e62e5c6e4b4354711d8c
ansarisuhel4505 <ansarisuhel4505@gmail.com>
2026-08-21 15:10:15 +0530

    Integrate Head component for site metadata
    Added Head component for favicon and theme color.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
   return (
@@ -8,3 +9,21 @@ export default function MyApp({ Component, pageProps }) {
     </ClerkProvider>
   );
 }
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* Google aur Browser ko Logo batane ka code */}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
+        <meta name="theme-color" content="#E5322D" />
+      </Head>
+      
+      {/* Tumhari saari website yahan load hoti hai */}
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
