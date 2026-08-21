b524600e603989cdf5f87871d7722f670c927e96
ansarisuhel4505 <ansarisuhel4505@gmail.com>
2026-08-21 16:51:10 +0530

    Add ThemeProvider for light/dark mode support

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,22 @@
 import '@/styles/globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import Head from 'next/head';
+import { ThemeProvider } from 'next-themes';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ClerkProvider {...pageProps}>
-      <Head>
-        {/* Google aur Browser ko Logo batane ka code */}
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="shortcut icon" href="/favicon.svg" />
-        <link rel="apple-touch-icon" href="/favicon.svg" />
-        <meta name="theme-color" content="#E5322D" />
-      </Head>
-      
-      {/* Tumhari saari website yahan load hoti hai */}
-      <Component {...pageProps} />
+      {/* 🌙 ThemeProvider site ko Light/Dark switch karne ki taqat deta hai */}
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <Head>
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+          <link rel="shortcut icon" href="/favicon.svg" />
+          <link rel="apple-touch-icon" href="/favicon.svg" />
+          <meta name="theme-color" content="#E5322D" />
+        </Head>
+        
+        <Component {...pageProps} />
+      </ThemeProvider>
     </ClerkProvider>
   );
 }
