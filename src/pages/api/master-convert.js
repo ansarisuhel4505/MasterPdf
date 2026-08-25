8d96c3cb7c3a29112ac17135097fcf003f7373d5
ansarisuhel4505 <ansarisuhel4505@gmail.com>
2026-08-25 14:49:19 +0530

    Update master-convert.js

--- src/pages/api/master-convert.js
+++ src/pages/api/master-convert.js
@@ -6,7 +6,7 @@ export const maxDuration = 60;
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' }); 
+    return res.status(405).json({ error: 'Method not allowed' });  
   }
 
   const { action, fileUrl, password, boxes, pageIndices } = req.body;
