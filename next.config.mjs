6a96b047578a9c637a63d3692bfbcb9d52e64055
ansarisuhel4505 <ansarisuhel4505@gmail.com>
2026-09-02 21:57:46 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,20 @@
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // Yeh poori website ke har page par apply hoga
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow' 
+          }
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
