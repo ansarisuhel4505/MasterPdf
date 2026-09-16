5cc53949848353d6d27bd748c339afad928afcea
ansarisuhel4505 <ansarisuhel4505@gmail.com>
2026-09-16 07:26:14 +0530

    Add ThemeProvider and restructure MyApp component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,22 @@
 import '@/styles/globals.css';
 import Head from 'next/head';
-import { SessionProvider } from "next-auth/react"; // 🔥 YEH NAYA IMPORT HAI
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from 'next-themes'; // 🔥 NAYA IMPORT
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    // 🔥 PURI APP KO SESSION PROVIDER KE ANDAR RAKH DIYA
     <SessionProvider session={session}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <meta name="theme-color" content="#E5322D" />
-      </Head>
-      
-      <Component {...pageProps} />
+      {/* 🔥 THEME PROVIDER ADD KIYA */}
+      <ThemeProvider attribute="class" defaultTheme="light">
+        <Head>
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <meta name="theme-color" content="#E5322D" />
+        </Head>
+        
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   );
 }
