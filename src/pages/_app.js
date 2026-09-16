75a309ab6578e41020ea5567b93bd94efbb4aa4f
ansarisuhel4505 <ansarisuhel4505@gmail.com>
2026-09-16 06:35:18 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,24 +1,19 @@
 import '@/styles/globals.css';
 import Head from 'next/head';
+import { SessionProvider } from "next-auth/react"; // 🔥 YEH NAYA IMPORT HAI
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <>
+    // 🔥 PURI APP KO SESSION PROVIDER KE ANDAR RAKH DIYA
+    <SessionProvider session={session}>
       <Head>
-        {/* 1. Standard ICO (Google Search Bot isko sabse pehle uthayega) */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        
-        {/* 2. Modern Browsers ke liye SVG */}
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        
-        {/* 3. Apple/Mobile devices ke liye (Hamesha PNG chahiye hota hai) */} 
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        
         <meta name="theme-color" content="#E5322D" />
       </Head>
       
-      {/* Tumhari saari website yahan load hoti hai */}
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
